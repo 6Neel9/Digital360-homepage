ee7c41d327051e92188280b122cf7c5d394bb696
6Neel9 <88641136+6Neel9@users.noreply.github.com>
2023-02-26 12:34:07 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <div style={{ display: "flex", flexDirection: "row" }}>
             <img alt="" style={{ width: "16.6%" }} src="https://5.imimg.com/data5/NR/EX/CC/SELLER-3619257/c-1-clip-hanger-500x500.jpg" />
-            <img alt="" src="https://www.shutterstock.com/image-photo/typical-wear-made-denim-260nw-731097895.jpg" />
+            <img alt="" style={{ width: "16.6%" }} src="https://www.shutterstock.com/image-photo/typical-wear-made-denim-260nw-731097895.jpg" />
             <img alt="" style={{ width: "16.6%" }} src="https://rukminim1.flixcart.com/image/612/612/xif0q/jean/8/m/j/38-1-hpskneeslit-white-urbano-fashion-original-imafzkgzgvnqtqpr-bb.jpeg?q=70" />
             <img alt="" style={{ width: "16.6%" }} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNmvuw-gdUt-sXXvCHl3GelsYLaNbupvwf_WNnw5md5vSsM4a6EXFePHrhh2ydVKwMqQ4&usqp=CAU" />
             <img alt="" style={{ width: "16.6%" }} src="https://images.herzindagi.info/image/2020/Jul/nail-hand-care-benefits.jpg" />
